3ee89191fd1f98ee2d9d9a2ffb2984e774517d45
vaiorabbit <vaiorabbit>
2020-07-18 15:08:25 +0900

    geometryc mesh loader

--- examples/ruby/common/mesh.rb
+++ examples/ruby/common/mesh.rb
@@ -9,44 +9,21 @@ module SampleMesh
   BGFX_CHUNK_MAGIC_IBC = 'I'.ord | ('B'.ord << 8) | ('C'.ord << 16) | 0x1 << 24
   BGFX_CHUNK_MAGIC_PRI = 'P'.ord | ('R'.ord << 8) | ('I'.ord << 16) | 0x0 << 24
 
-  class Sphere
-    attr_accessor :center, :radius
-    def initialize
-      @center = RMath3D::RVec3.new
-      @radius = 0.0
-    end
-  end
-
-  class SphereBuf < FFI::Struct
+  class Sphere < FFI::Struct
     layout(
       :center, [:float, 3],
       :radius, :float
     )
   end
 
-  class Aabb
-    attr_accessor :min, :max
-    def initialize
-      @min = RMath3D::RVec3.new
-      @max = RMath3D::RVec3.new
-    end
-  end
-
-  class AabbBuf < FFI::Struct
+  class Aabb < FFI::Struct
     layout(
       :min, [:float, 3],
       :max, [:float, 3]
     )
   end
 
-  class Obb
-    attr_accessor :max
-    def initialize
-      @mtx = Array.new(16) { 0.0 }
-      end
-  end
-
-  class ObbBuf < FFI::Struct
+  class Obb < FFI::Struct
     layout(
       :mtx, [:float, 16]
     )
@@ -61,9 +38,9 @@ module SampleMesh
       @m_numIndices = 0
       @m_startVertex = 0
       @m_numVertices = 0
-      @m_sphere = SphereBuf.new
-      @m_aabb = AabbBuf.new
-      @m_obb = ObbBuf.new
+      @m_sphere = Sphere.new
+      @m_aabb = Aabb.new
+      @m_obb = Obb.new
     end
   end
 
@@ -72,26 +49,16 @@ module SampleMesh
     attr_accessor :m_sphere, :m_aabb, :m_obb, :m_prims
 
     def initialize
-      @m_vbh = Bgfx_vertex_buffer_handle_t.new
-      @m_ibh = Bgfx_index_buffer_handle_t.new
+      @m_vbh = nil # Bgfx_vertex_buffer_handle_t
+      @m_ibh = nil # Bgfx_index_buffer_handle_t
       @m_numVertices = 0
       @m_vertices = nil
       @m_numIndices = 0
       @m_indices = nil
-      @m_sphere = SphereBuf.new(FFI::MemoryPointer.new(:uint8, SphereBuf.size, false))
-      @m_aabb = AabbBuf.new(FFI::MemoryPointer.new(:uint8, AabbBuf.size, false))
-      @m_obb = ObbBuf.new(FFI::MemoryPointer.new(:uint8, ObbBuf.size, false))
+      @m_sphere = Sphere.new
+      @m_aabb = Aabb.new
+      @m_obb = Obb.new#
       @m_prims = [] # array of Primitive
-    end
-
-    def reset
-      @m_vbh[:idx] = Bgfx::InvalidHandleIdx
-      @m_ibh[:idx] = Bgfx::InvalidHandleIdx
-      @m_numVertices = 0
-      @m_vertices = nil
-      @m_numIndices = 0
-      @m_indices = nil
-      @m_prims.clear
     end
   end
 
@@ -129,46 +96,35 @@ module SampleMesh
       @@id_to_attrib[id]
     end
 
-
     def self.id_to_attrib_type(id)
       @@id_to_attrib_type[id]
     end
 
     def self.read(io, layout)
       total = 0
-      num_attrs = FFI::MemoryPointer.new(:uint8, 1, false).write_string(io.read(FFI.type_size(:uint8)))
-      total += num_attrs.size
-      stride = FFI::MemoryPointer.new(:uint16, 1, false).write_string(io.read(FFI.type_size(:uint16)))
-      total += stride.type_size
-
-      offset = FFI::MemoryPointer.new(:uint16, 1, false)
-      attrib_id = FFI::MemoryPointer.new(:uint16, 1, false)
-      num = FFI::MemoryPointer.new(:uint8, 1, false)
-      attrib_type_id = FFI::MemoryPointer.new(:uint16, 1, false)
-      normalized = FFI::MemoryPointer.new(:bool, 1, false)
-      as_int = FFI::MemoryPointer.new(:bool, 1, false)
+      num_attrs = io.read(FFI.type_size(:uint8)).unpack1("C");      total += FFI.type_size(:uint8)
+      stride = io.read(FFI.type_size(:uint16)).unpack1("S");        total += FFI.type_size(:uint16)
 
       layout.begin
-      num_attrs.read_uint8.times do |_ii|
-        offset.write_string(io.read(FFI.type_size(:uint16)))
-        attrib_id.write_string(io.read(FFI.type_size(:uint16)))
-        num.write_string(io.read(FFI.type_size(:uint8)))
-        attrib_type_id.write_string(io.read(FFI.type_size(:uint16)))
-        normalized.write_string(io.read(FFI.type_size(:bool)))
-        as_int.write_string(io.read(FFI.type_size(:bool)))
-        total += (offset.size + attrib_id.size + num.size + attrib_type_id.size + normalized.size + as_int.size)
+      num_attrs.times do
+        offset = io.read(FFI.type_size(:uint16)).unpack1("S");           total += FFI.type_size(:uint16)
+        attrib_id = io.read(FFI.type_size(:uint16)).unpack1("S");        total += FFI.type_size(:uint16)
+        num = io.read(FFI.type_size(:uint8)).unpack1("C");               total += FFI.type_size(:uint8)
+        attrib_type_id = io.read(FFI.type_size(:uint16)).unpack1("S");   total += FFI.type_size(:uint16)
+        normalized = io.read(FFI.type_size(:bool)).unpack1("C") != 0;    total += FFI.type_size(:bool)
+        as_int = io.read(FFI.type_size(:bool)).unpack1("C") != 0;        total += FFI.type_size(:bool)
 
-        attrib = id_to_attrib(attrib_id.read_uint16)
-
-        type = id_to_attrib_type(attrib_type_id.read_uint16)
+        attrib = id_to_attrib(attrib_id)
+        type = id_to_attrib_type(attrib_type_id)
         if attrib != Bgfx::Attrib::Count && type != Bgfx::Attrib::Count
-          layout.add(attrib, num.read(:uint8), type, normalized.read(:bool), as_int.read(:bool))
-          layout[:offset][attrib] = offset.read(:uint16)
+          layout.add(attrib, num, type, normalized, as_int)
+          layout[:offset][attrib] = offset
         end
       end
       layout.end
-      layout[:stride] = stride.read_uint16
-      total
+      layout[:stride] = stride
+
+      return total
     end
   end
 
@@ -178,7 +134,7 @@ module SampleMesh
 
     def initialize
       @instance = nil
-      @m_layout = Bgfx_vertex_layout_t.new # (FFI::MemoryPointer.new(:uint8, Bgfx_vertex_layout_t.size, false))
+      @m_layout = Bgfx_vertex_layout_t.new
       @m_groups = []
     end
 
@@ -188,13 +144,9 @@ module SampleMesh
       while io.read(4, chunk)
         case chunk.unpack1('L')
         when BGFX_CHUNK_MAGIC_VB
-          group.m_sphere.pointer.write_string(io.read(SphereBuf.size))
-          group.m_aabb.pointer.write_string(io.read(AabbBuf.size))
-          group.m_obb.pointer.write_string(io.read(ObbBuf.size))
-
-          # puts("Sphere #{group.m_sphere[:radius]}, #{group.m_sphere[:center][0]}, #{group.m_sphere[:center][1]}, #{group.m_sphere[:center][2]}")
-          # puts("Aabb #{group.m_aabb[:min][0]},#{group.m_aabb[:min][1]},#{group.m_aabb[:min][2]},  #{group.m_aabb[:max][0]},#{group.m_aabb[:max][1]},#{group.m_aabb[:max][2]}")
-          # puts("Obb #{group.m_obb[:mtx].to_a}")
+          group.m_sphere.pointer.write_string(io.read(Sphere.size))
+          group.m_aabb.pointer.write_string(io.read(Aabb.size))
+          group.m_obb.pointer.write_string(io.read(Obb.size))
 
           SampleMesh::VertexLayout.read(io, @m_layout)
 
@@ -203,35 +155,31 @@ module SampleMesh
           # stride = @m_layout.get_stride()
           stride = @m_layout[:stride]
           group.m_numVertices = FFI::MemoryPointer.new(:uint16, 1, false).write_string(io.read(FFI.type_size(:uint16)))
-          raw_mem = Bgfx.alloc(group.m_numVertices.read_uint16 * stride)
-          mem = Bgfx_memory_t.new(raw_mem) # TODO: make Bgfx::alloc directly return Bgfx_memory_t instance
+          mem = Bgfx_memory_t.new(Bgfx.alloc(group.m_numVertices.read_uint16 * stride)) # TODO: make Bgfx::alloc directly return Bgfx_memory_t instance
           mem[:data].write_string(io.read(mem[:size]))
           # TODO: if ramcopy == true
           group.m_vbh = Bgfx.create_vertex_buffer(mem, m_layout)
 
         when BGFX_CHUNK_MAGIC_VBC
           pp "[TODO] VBC 0x#{chunk.unpack1('L').to_s(16)}"
-          # TODO
+
         when BGFX_CHUNK_MAGIC_IB
-          # pp "IB 0x#{chunk.unpack1("L").to_s(16)}"
           group.m_numIndices = FFI::MemoryPointer.new(:uint32, 1, false).write_string(io.read(FFI.type_size(:uint32)))
-          raw_mem = Bgfx.alloc(group.m_numIndices.read_uint32 * 2)
-          mem = Bgfx_memory_t.new(raw_mem) # TODO: make Bgfx::alloc directly return Bgfx_memory_t instance
+          mem = Bgfx_memory_t.new(Bgfx.alloc(group.m_numIndices.read_uint32 * 2)) # TODO: make Bgfx::alloc directly return Bgfx_memory_t instance
           mem[:data].write_string(io.read(mem[:size]))
           # TODO: if ramcopy == true
           group.m_ibh = Bgfx.create_index_buffer(mem)
 
         when BGFX_CHUNK_MAGIC_IBC
           pp "[TODO] IBC 0x#{chunk.unpack1('L').to_s(16)}"
-          # TODO
+
         when BGFX_CHUNK_MAGIC_PRI
-          # pp "PRI 0x#{chunk.unpack1("L").to_s(16)}"
           len = io.read(FFI.type_size(:uint16)).unpack1('S')
           material = ' ' * len
           io.read(len, material)
 
           num = io.read(FFI.type_size(:uint16)).unpack1('S')
-          num.times do |_ii|
+          num.times do
             len = io.read(FFI.type_size(:uint16)).unpack1('S')
             name = ' ' * len
             io.read(len, name)
@@ -241,9 +189,9 @@ module SampleMesh
             prim.m_numIndices = io.read(FFI.type_size(:uint32)).unpack1('L')
             prim.m_startVertex = io.read(FFI.type_size(:uint32)).unpack1('L')
             prim.m_numVertices = io.read(FFI.type_size(:uint32)).unpack1('L')
-            prim.m_sphere.pointer.write_string(io.read(SphereBuf.size))
-            prim.m_aabb.pointer.write_string(io.read(AabbBuf.size))
-            prim.m_obb.pointer.write_string(io.read(ObbBuf.size))
+            prim.m_sphere.pointer.write_string(io.read(Sphere.size))
+            prim.m_aabb.pointer.write_string(io.read(Aabb.size))
+            prim.m_obb.pointer.write_string(io.read(Obb.size))
 
             group.m_prims << prim
           end
